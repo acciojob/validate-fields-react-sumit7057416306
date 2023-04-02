1dd0950a3a286c6eff335919d9f6bd3da2fc38e6
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:12:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+function App() {
+ const [username, setUsername] = useState("");
+ const [password, setPassword] = useState("");
+ const [errorMessage, setErrorMessage] = useState("");
+
+ const handleSubmit = (event) => {
+   event.preventDefault();
+   if (!username || !password) {
+     setErrorMessage("Both username and password are required.");
+   } else {
+     setErrorMessage("");
+     // Perform actual login logic here...
+   }
+ };
+
+ return (
+   <form onSubmit={handleSubmit}>
+     <p>
+       <label htmlFor="username">Username:</label>
+       <input
+         type="text"
+         id="username"
+         value={username}
+         onChange={(event) => setUsername(event.target.value)}
+       />
+     </p>
+     <p>
+       <label htmlFor="password">Password:</label>
+       <input
+         type="password"
+         id="password"
+         value={password}
+         onChange={(event) => setPassword(event.target.value)}
+       />
+     </p>
+     {errorMessage && <p id='errorMessage' style={{ color: "red" }}>{errorMessage}</p>}
+     <button type="submit">Login</button>
+   </form>
+ );
 }
 
-export default App
+export default App;
